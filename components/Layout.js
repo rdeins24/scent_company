a3d4d32a9062221bb863b58aa101ce4e3f52b14c
Denis Rucan <rdenis24@yahoo.com>
2024-07-26 18:33:31 +0100

    Fixed layout and added white space adjustments

--- components/Layout.js
+++ components/Layout.js
@@ -1,16 +1,24 @@
 import Footer1 from "./Footer1";
 import Navbar2 from "./Navbar2";
+import Head from "next/head";
 
 export default function Layout({ children }) {
   return (
-    <html lang="en">
-      <body>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Scentio Milano</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
+        />
+      </Head>
+      <div className="">
         <Navbar2 />
-        <div> {/* Adjust px values as needed */}
-          {children}
-        </div>
+        <main>{children}</main>
         <Footer1 />
-      </body>
-    </html>
+      </div>
+    </>
   );
 }
