be5cf03c293bbd7fcf033249e8e6170e4fdf841f
Denis Rucan <rdenis24@yahoo.com>
2024-08-29 14:51:34 +0100

    changes on mobile version

--- components/Main/Gallery.js
+++ components/Main/Gallery.js
@@ -5,8 +5,6 @@ import img from "../../public/assets/img/building.jpg";
 import img2 from "../../public/assets/img/dif.jpg";
 import img3 from "../../public/assets/slider/blur.jpg";
 import img4 from "../../public/assets/slider/close.jpg";
-import img5 from "../../public/assets/slider/light.jpg";
-import img6 from "../../public/assets/slider/pikaso.jpg";
 import img7 from "../../public/assets/slider/sunlight.jpg";
 import img8 from "../../public/assets/slider/vertical.jpg";
 import img9 from "../../public/assets/slider/vivid.jpg";
@@ -43,9 +41,106 @@ const CustomSlider = () => {
   }, []);
 
   return (
-    <div className="bg-black md:py-40 pb-[90px]  relative">
+    <div className="bg-black md:py-40 pb-[90px] relative">
+      {/* Content Section */}
+      <div className="flex flex-col md:flex-row">
+        {/* Static Text Block */}
+        <div className="w-full md:w-[30%] text-white bg-black px-10 p-10">
+          <h2 className="text-xl font-bold">Definition of an Odotype</h2>
+          <p className="md:mt-4">
+            An odotype is a unique scent that identifies a brand or
+            establishment. We have a unique method to create the perfect
+            odotype...
+          </p>
+        </div>
+
+        {/* Custom Slider */}
+        <div className="w-full md:w-[70%] shadow-2xl">
+          <div
+            ref={sliderRef}
+            className="flex space-x-2 overflow-hidden scroll-smooth touch-pan-x"
+            style={{ scrollSnapType: "x mandatory" }}
+          >
+            <div className="relative w-full md:w-[30%] shrink-0 md:h-[600px]">
+              <Image
+                src={img}
+                alt="Slide 1"
+                className="object-cover w-full h-60 md:h-full opacity-70"
+              />
+              <div className="absolute inset-0 flex items-center justify-center">
+                <h2 className="text-white text-sm font-semibold m-10 md:text-left md:bottom-0 md:mb-4">
+                  1An odotype is a unique scent that identifies a brand or
+                  establishment. We have a unique method to create the perfect
+                  odotype...
+                </h2>
+              </div>
+            </div>
+
+            <div className="relative w-full md:w-[30%] shrink-0 md:h-[600px]">
+              <Image
+                src={img3}
+                alt="Slide 3"
+                className="object-cover w-full h-60 md:h-full opacity-70"
+              />
+              <div className="absolute inset-0 flex items-center justify-center">
+                <h2 className="text-white text-sm font-semibold m-10 md:text-left md:bottom-0 md:mb-4">
+                  2An odotype is a unique scent that identifies a brand or
+                  establishment. We have a unique method to create the perfect
+                  odotype...
+                </h2>
+              </div>
+            </div>
+
+            <div className="relative w-full md:w-[30%] shrink-0 md:h-[600px]">
+              <Image
+                src={img4}
+                alt="Slide 4"
+                className="object-cover w-full h-60 md:h-full opacity-70"
+              />
+              <div className="absolute inset-0 flex items-center justify-center">
+                <h2 className="text-white text-sm font-semibold m-10 md:text-left md:bottom-0 md:mb-4">
+                  3An odotype is a unique scent that identifies a brand or
+                  establishment. We have a unique method to create the perfect
+                  odotype...
+                </h2>
+              </div>
+            </div>
+
+            <div className="relative w-full md:w-[30%] shrink-0 md:h-[600px]">
+              <Image
+                src={img8}
+                alt="Slide 4"
+                className="object-cover w-full h-60 md:h-full opacity-70"
+              />
+              <div className="absolute inset-0 flex items-center justify-center">
+                <h2 className="text-white text-sm font-semibold m-10 md:text-left md:bottom-0 md:mb-4">
+                  An odotype is a unique scent that identifies a brand or
+                  establishment. We have a unique method to create the perfect
+                  odotype...
+                </h2>
+              </div>
+            </div>
+
+            <div className="relative w-full md:w-[30%] shrink-0 md:h-[600px]">
+              <Image
+                src={img9}
+                alt="Slide 4"
+                className="object-cover w-full h-60 md:h-full opacity-70"
+              />
+              <div className="absolute inset-0 flex items-center justify-center">
+                <h2 className="text-white text-sm font-semibold m-10 md:text-left md:bottom-0 md:mb-4">
+                  An odotype is a unique scent that identifies a brand or
+                  establishment. We have a unique method to create the perfect
+                  odotype...
+                </h2>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
       {/* Navigation Buttons */}
-      <div className="flex justify-end space-x-4  z-10 absolute top-[520px] right-[100px] sm:top-14">
+      <div className="flex justify-center mt-4 md:mt-0 space-x-4 z-10 md:absolute md:top-[50px] md:right-[150px]">
         <button
           onClick={handlePrev}
           className={`p-3 rounded-full transition-all ${
@@ -74,132 +169,6 @@ const CustomSlider = () => {
             }`}
           />
         </button>
-      </div>
-
-      {/* Content Section */}
-      <div className="flex flex-col md:flex-row">
-        {/* Static Text Block */}
-        <div className="w-full md:w-[30%] text-white bg-black px-10 p-10">
-          <h2 className="text-xl font-bold">Definition of an Odotype</h2>
-          <p className="md:mt-4">
-            An odotype is a unique scent that identifies a brand or
-            establishment. We have a unique method to create the perfect
-            odotype...
-          </p>
-        </div>
-
-        {/* Custom Slider */}
-        <div className="w-full md:w-[70%] shadow-2xl ">
-          <div
-            ref={sliderRef}
-            className="flex space-x-2 overflow-hidden scroll-smooth touch-pan-x"
-            style={{ scrollSnapType: "x mandatory" }}
-          >
-            <div className="relative w-full md:w-[30%] shrink-0 md:h-[600px]">
-              <Image
-                src={img}
-                alt="Slide 1"
-                className="object-cover w-full h-60 md:h-full opacity-70"
-              />
-              <div className="absolute inset-0 flex items-center justify-center ">
-                <h2 className="text-white text-sm font-semibold  m-10 md:text-left md:bottom-0 md:mb-4 ">
-                1An odotype is a unique scent that identifies a brand or
-                  establishment. We have a unique method to create the perfect
-                  odotype...      1An odotype is a unique scent that identifies a brand or
-                  establishment. We have a unique method to create the perfect
-                  odotype...
-                </h2>
-              </div>
-            </div>
-
-            <div className="relative w-full md:w-[30%] shrink-0 md:h-[600px] ">
-              <Image
-                src={img3}
-                alt="Slide 3"
-                className="object-cover w-full h-60 md:h-full opacity-70"
-              />
-              <div className="absolute inset-0 flex items-center justify-center">
-                <h2 className="text-white text-sm font-semibold  m-10 md:text-left md:bottom-0 md:mb-4 ">
-                  {" "}
-                  2An odotype is a unique scent that identifies a brand or
-                  establishment. We have a unique method to create the perfect
-                  odotype...
-                  2An odotype is a unique scent that identifies a brand or
-                  establishment. We have a unique method to create the perfect
-                  odotype...
-                </h2>
-              </div>
-            </div>
-            <div className="relative w-full md:w-[30%] shrink-0 md:h-[600px] ">
-              <Image
-                src={img4}
-                alt="Slide 4"
-                className="object-cover w-full h-60 md:h-full opacity-70"
-              />
-              <div className="absolute inset-0 flex items-center justify-center">
-                <h2 className="text-white text-sm font-semibold  m-10 md:text-left md:bottom-0 md:mb-4 ">
-                  {" "}
-                  3An odotype is a unique scent that identifies a brand or
-                  establishment. We have a unique method to create the perfect
-                  odotype...   3An odotype is a unique scent that identifies a brand or
-                  establishment. We have a unique method to create the perfect
-                  odotype...
-                </h2>
-              </div>
-            </div>
-            {/* <div className="relative w-full md:w-[30%] shrink-0 md:h-[600px] ">
-              <Image
-                src={img7}
-                alt="Slide 4"
-                className="object-cover w-full h-60 md:h-full md:opacity-70"
-              />
-              <div className="md:absolute md:inset-0 flex items-center justify-center md:p-4">
-                <h2 className="text-white text-sm font-semibold mt-[-100px] m-10 md:text-left md:bottom-0 md:mb-4 ">
-                  {" "}
-                  PRAn odotype is a unique scent that identifies a brand or
-                  establishment. We have a unique method to create the perfect
-                  odotype...     An odotype is a unique scent that identifies a brand or
-                  establishment. We have a unique method to create the perfect
-                  odotype...
-                </h2>
-              </div>
-            </div> */}
-            <div className="relative w-full md:w-[30%] shrink-0 md:h-[600px]  ">
-              <Image
-                src={img8}
-                alt="Slide 4"
-                className="object-cover w-full h-60 md:h-full opacity-70"
-              />
-              <div className="absolute inset-0 flex items-center justify-center">
-                <h2 className="text-white text-sm font-semibold  m-10 md:text-left md:bottom-0 md:mb-4 ">
-                  {" "}
-                  An odotype is a unique scent that identifies a brand or
-                  establishment. We have a unique method to create the perfect
-                  odotype...     An odotype is a unique scent that identifies a brand or
-                  establishment. We have a unique method to create the perfect
-                  odotype...
-                </h2>
-              </div>
-            </div>
-            <div className="relative w-full md:w-[30%] shrink-0 md:h-[600px]  ">
-              <Image
-                src={img9}
-                alt="Slide 4"
-                className="object-cover w-full h-60 md:h-full opacity-70"
-              />
-              <div className="absolute inset-0 flex items-center justify-center">
-                <h2 className="text-white text-sm font-semibold  m-10 md:text-left md:bottom-0 md:mb-4 ">
-                  {" "}
-                  An odotype is a unique scent that identifies a brand or
-                  establishment. We have a unique method to create the perfect
-                  odotype...     An odotype is a unique scent that identifies a brand or
-                  establishment. We have a unique method to create the perfect
-                  odotype...
-                </h2>
-              </div>
-            </div>
-          </div>
-        </div>
       </div>
     </div>
   );
--- components/services.tsx
+++ components/services.tsx
@@ -2,14 +2,6 @@ import React, { useContext, useRef, useState, useEffect } from "react";
 import s from "../styles/skills.module.css";
 import { ScrollContext } from "../utils/scroll-observer";
 import Link from "next/link";
-import Image from "next/image";
-
-// Import your images here
-import bgImage1 from "../public/assets/slider/vivid.jpg"; 
-import bgImage2 from "../public/assets/slider/vertical.jpg";
-import bgImage3 from "../public/assets/slider/pikaso.jpg";
-import bgImage4 from "../public/assets/slider/vertical.jpg";
-import bgImage5 from "../public/assets/img/pink.jpg";
 
 const opacityForBlock = (sectionProgress: number, blockNo: number) => {
   const progress = sectionProgress - blockNo;
@@ -45,38 +37,29 @@ const Skills: React.FC = () => {
     }
   }, [progress, activeService]);
 
-  // Array of background images
-  const backgroundImages = [
-    bgImage1, // Hotels
-    bgImage2, // Restaurants
-    bgImage3, // Automotive
-    bgImage4, // Showroom
-    bgImage5, // Retail
+  const backgroundColors = [
+    "bg-gradient-to-bl from-black via-gray-200 to-orange-500",
+    "bg-gradient-to-tr from-black via-blue-500 via-5% to-orange-500",
+    "bg-gradient-to-r from-red-200 to-yellow-200",
+    "bg-red-200",
+    "bg-blue-400",
+    "bg-gray-500",
+    "bg-gradient-to-r from-teal-400 to-gray-800"
   ];
 
   return (
     <div
       ref={refContainer}
-      className="transition-colors duration-700 relative"
+      className={`transition-colors duration-300 ${backgroundColors[activeService]}`}
     >
-      {/* Render the active background image */}
-      <Image
-        src={backgroundImages[activeService]}
-        alt="Background"
-        layout="fill"
-        objectFit="cover"
-        className="absolute inset-0 z-0 opacity-80 blur-2xl transition-opacity duration-[1500ms] ease-out"
-      />
-      <div className="absolute inset-0 bg-black opacity-30 z-1 transition-opacity duration-[1500ms] ease-out"></div>
-      
-      <div className="relative z-10 min-h-screen max-w-5xl mx-auto px-10 lg:px-20 py-24 md:py-28 lg:py-36 flex flex-col justify-center items-center text-4xl md:text-6xl lg:text-6xl tracking-tight font-semibold text-black">
-        <div className="leading-[1.25] space-y-50">
-          {["Hotele", "Restaurante", "Automotive", "Showroom-uri", "Retail"].map(
+      <div className="min-h-screen max-w-5xl mx-auto px-10 lg:px-20 py-24 md:py-28 lg:py-36 flex flex-col justify-center items-center text-4xl md:text-6xl lg:text-6xl tracking-tight font-semibold text-black">
+        <div className="leading-[1.25] space-y-20">
+          {["Hotels", "Restaurants", "Automotive", "Showroom", "Retail"].map(
             (service, index) => (
               <Link href={`/services/${service.toLowerCase()}`} key={index}>
                 <div
-                  className={`${s.skillText} cursor-pointer my-20 transition-all duration-700 ease-out ${
-                    activeService === index ? "font-semibold underline md:text-6xl" : ""
+                  className={`${s.skillText} cursor-pointer my-10 transition-all duration-300 ${
+                    activeService === index ? "font-bold " : ""
                   }`}
                   style={{
                     opacity: opacityForBlock(progress, index),
